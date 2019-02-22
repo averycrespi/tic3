126e6c051b39144cc52725c4b7d558f104a748c6
Avery Crespi <averycrespi@gmail.com>
2019-02-21 20:06:04 -0800

    Refactor overseer

--- tic3/Assets/Scripts/Overseer.cs
+++ tic3/Assets/Scripts/Overseer.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 public class Overseer : MonoBehaviour
 {
     public static Overseer instance = null;
+
     public GameObject board;
     public Material normal;
     public Material normalBlue;
@@ -11,7 +12,7 @@ public class Overseer : MonoBehaviour
 
     private BoardController controller;
     private int legalIndex;
-    private bool redTurn;
+    private bool isRedTurn;
 
     void Awake()
     {
@@ -24,45 +25,36 @@ public class Overseer : MonoBehaviour
             Destroy(gameObject);
         }
 
-        Debug.Log("Starting overseer");
         controller = board.GetComponent<BoardController>();
         controller.InitializeBoard();
         legalIndex = -1;
-        redTurn = true;
+        isRedTurn = true;
     }
 
     Tuple<int, int> ParseName(string name)
     {
-        //TODO: handle errors
         string[] parts = name.Split(',');
         return new Tuple<int, int>(int.Parse(parts[0]), int.Parse(parts[1]));
     }
 
     void TakeTurn(int superIndex, int subIndex)
     {
-        if (redTurn)
-        {
-            controller.SetCubeMaterial(superIndex, subIndex, normalRed);
-            redTurn = false;
-        }
-        else
-        {
-            controller.SetCubeMaterial(superIndex, subIndex, normalBlue);
-            redTurn = true;
-        }
+        Material m = isRedTurn ? normalRed : normalBlue;
+        controller.SetCubeMaterial(superIndex, subIndex, m);
+        isRedTurn = !isRedTurn;
     }
   
-    void AdvanceBoard(int subIndex)
+    void AdvanceBoard(int superIndex)
     {
-        if (controller.IsFull(subIndex))
+        if (controller.IsFull(superIndex))
         {
             controller.UnhideAll();
             legalIndex = -1;
         }
         else
         {
-            controller.HideAllExcept(subIndex);
-            legalIndex = subIndex;
+            controller.HideAllExcept(superIndex);
+            legalIndex = superIndex;
         }
     }
 
@@ -71,7 +63,6 @@ public class Overseer : MonoBehaviour
         Tuple<int, int> indexes = ParseName(cube.name);
         int superIndex = indexes.Item1;
         int subIndex = indexes.Item2;
-        Debug.Log("Handing click: superIndex=" + superIndex.ToString() + ", subIndex=" + subIndex.ToString());
 
         Material current = cube.GetComponent<Renderer>().sharedMaterial;
         if ((legalIndex == -1 || superIndex == legalIndex) && current == normal)
