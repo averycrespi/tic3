590a65750d5518748e6a2d03ac3be1ea402e3d54
Avery Crespi <averycrespi@gmail.com>
2019-02-22 10:25:24 -0800

    Add reset button

---
+++ tic3/Assets/Scripts/UI/ResetButtonController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class ResetButtonController : MonoBehaviour
+{
+    public Button resetButton;
+
+    private void Start()
+    {
+        resetButton.onClick.AddListener(ResetGame);
+    }
+
+    private void ResetGame()
+    {
+        SceneManager.LoadScene("MainScene");
+    }
+}
